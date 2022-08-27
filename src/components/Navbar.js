df4c733aa3117192ca97a2b3ea6067bcc33705f1
SittikronNon <sittikron.lamulsab@gmail.com>
2022-08-27 23:47:46 +0700

    Navbar setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <ul>
+            <li>Home</li>
+            <li>Project</li>
+            <li>Contract</li>
+            <li>Profile??</li>
+        </ul>
+    </>
+  )
+}
+
+export default Navbar
