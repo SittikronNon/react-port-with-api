b4b8514ba047f267eb8b5f0c706db91b2f699993
SittikronNon <sittikron.lamulsab@gmail.com>
2022-09-12 01:03:22 +0700

    about setup

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,40 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <div className='container mx-auto flex px-10 py-20 md:flex-row flex-col items-center'>
+        <div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
+          <h1 className='title-font sm:text-4xl text-3xl mb-4 font-medium text-white'>
+            Hi, I'm Non.
+            <br className='hidden lg:inline-block' />I love to build amazing apps.
+          </h1>
+          <p className='mb-8 leading-relaxed'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+            Laborum, voluptas natus? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+            Laborum, voluptas natus?
+          </p>
+          <div className='flex justify-center'>
+            <a href='#contact'
+            className='inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg'>
+            Work with me
+            </a>
+            <a href='#projects'
+            className='ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg'>
+            See My Past Work
+            </a>
+          </div>
+        </div>
+        <div className='lg:max-w-lg lg:w-full md:w-1/2 w-5/6'>
+          <img 
+            className='object-cover object-center rounded'
+            alt='hero'
+            src='https://images.unsplash.com/photo-1661961112835-ca6f5811d2af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80'
+          />
+        </div>
+      </div>
+    </section>
   )
 }
 
