90a73c1b434247f4362ae861bdc67d46d9928b57
SittikronNon <sittikron.lamulsab@gmail.com>
2022-09-13 02:43:51 +0700

    improve
    -map and contact update
    -placeholder for  my info
    -some projects on the page

---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const projects = [
+    {
+        title: "Augmented Reality for Displaying Artifacts of Southeast Asia Ceramic Museum",
+        subtitle: "AR museum",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum ",
+        image: "https://cdn.discordapp.com/attachments/425596377469550605/1018843293452337152/project-1.gif",
+        link: "https://github.com/SittikronNon/ARforBUmuseum",
+      },
+      {
+        title: "Escape The Station",
+        subtitle: "Platformer PC game",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "https://cdn.discordapp.com/attachments/425596377469550605/1018833253001592882/unknown.png",
+        link: "https://github.com/SittikronNon/EscapeTheStation",
+      },
+      {
+        title: "Landing Page",
+        subtitle: "learning reactsjs",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "https://media.discordapp.net/attachments/425596377469550605/1018846279939399710/unknown.png?width=1237&height=702",
+        link: "https://github.com/SittikronNon/landing-page",
+      },
+      
+]
---
+++ src/skills.js
@@ -0,0 +1,29 @@
+export const skills = [
+    {
+        skill: 'HTML/CSS'
+    },
+    {
+        skill: 'javascript'
+    },
+    {
+        skill: 'C#'
+    },
+    {
+        skill: 'C++'
+    },
+    {
+        skill: 'MongoDB'
+    },
+    {
+        skill: 'React JS'
+    },
+    {
+        skill: 'Nodejs'
+    },
+    {
+        skill: 'Bootstrap'
+    },
+    {
+        skill: 'Unity'
+    },
+]
